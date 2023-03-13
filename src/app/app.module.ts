4eeac389d19f2ea8f9147959eb43be4df46d8973
EDJAM2000 <124722900+EDJAM2000@users.noreply.github.com>
2023-03-13 14:03:58 -0500

    13/03/2023
    Se agregaron las librerías y se vinculo con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,9 @@ import { PortafolioComponent } from './content/portafolio/portafolio.component';
 import { DocenteComponent } from './content/docente/docente.component';
 import { PreciosComponent } from './content/precios/precios.component';
 import { CursosComponent } from './content/cursos/cursos.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule} from '@angular/fire/compat';
+
 
 const appRoutes: Routes=[
   { path:'', component: InicioComponent},
@@ -45,8 +48,17 @@ const appRoutes: Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes,{enableTracing:true})
-    
+    RouterModule.forRoot(appRoutes,{enableTracing:true}),
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAp2qXxVJeHtPCAQEDbbD167t-4TRRHrnM",
+  authDomain: "aprendebasickids.firebaseapp.com",
+  projectId: "aprendebasickids",
+  storageBucket: "aprendebasickids.appspot.com",
+  messagingSenderId: "965985004580",
+  appId: "1:965985004580:web:db9b259e6a6638eb553cb1",
+  measurementId: "G-22KSDS79MY"
+    })
   ],
   providers: [
     CargarscriptsService
